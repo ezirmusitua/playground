a08f97033db77c11648aa938939d78efaa667d6d
ezirmusitua <jferroal@gmail.com>
2017-07-13 09:45:55 +0800

    Add Range exercise

--- iterator-generator/iterator.js
+++ iterator-generator/iterator.js
@@ -32,8 +32,36 @@ class Iterator {
         if (this.iterable.getValue) return [index, this.iterable.getValue(index)];
         return [this.keys[index], this.iterable[this.keys[index] || index]];
     }
-
 }
+
+class Range {
+    constructor(low, high) {
+        this.low = low;
+        this.high = high;
+    }
+
+    [Symbol.iterator]() {
+        return new RangeIterator(this);
+    }
+}
+
+class RangeIterator {
+    constructor(range) {
+        this.range = range;
+        this.current = range.low;
+    }
+    next() {
+        const nextVal = {done: true};
+        if (this.current <= this.range.high) {
+            nextVal.value = this.current;
+            nextVal.done = false;
+            this.current += 1;
+        }
+        return nextVal;
+    }
+}
+
+
 function testIterator() {
     const lang = {Javascript: 1, Python: 2, 'C++': 3};
     const it1 = new Iterator(lang);
@@ -58,4 +86,11 @@ function testIterator() {
     }
 }
 
+function testRange() {
+    const range = new Range(3, 5);
+    for (const i of range)
+        console.log(i); // prints 3, then 4, then 5 in sequence
+}
+
 testIterator();
+testRange();
