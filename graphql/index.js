ec4d2f8ad3da75d8eba5ce4f5d7e0e5253f8132c
ezirmusitua <jferroal@gmail.com>
2017-09-08 17:40:19 +0800

    Update route to handle graphql query
    Took 8 minutes

--- graphql/index.js
+++ graphql/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const graphql = require('graphql').graphql;
+const bodyParser = require('body-parser');
 
+const schema = require('./schema');
+
+// === constants
 const PORT = 3002;
 const HOST = '127.0.0.1';
+
+// === express app
 const app = express();
 
+// === middlewares
+app.use(bodyParser.text({ type: 'application/graphql' }));
+
+// === routes
 app.post('/graphql', (req, res) => {
-    res.send('hello');
+    graphql(schema, req.body).then((query) => {
+        res.send(JSON.stringify(query, null, null));
+    });
 });
 
+// === start application
 app.listen(PORT, HOST, () => {
     console.log('GraphQL Server is listening at http://%s:%s', HOST, PORT);
 });
