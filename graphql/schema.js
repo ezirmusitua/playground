85f9832f6e2874edf5ad70eb48adb8bea342d766
ezirmusitua <jferroal@gmail.com>
2017-09-08 17:39:57 +0800

    Add graphql schema
    Took 7 minutes

---
+++ graphql/schema.js
@@ -0,0 +1,19 @@
+const { GraphQLObjectType, GraphQLSchema, GraphQLInt } = require('graphql');
+
+let count = 0;
+
+let schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {
+            count: {
+                type: GraphQLInt,
+                resolve: function () {
+                    return count;
+                },
+            },
+        },
+    }),
+});
+
+module.exports = schema;
