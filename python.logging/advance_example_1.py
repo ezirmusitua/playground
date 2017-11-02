6fdade603797c6de941a81849d03cae6c74fa9e0
ezirmusitua <jferroal@gmail.com>
2017-11-02 08:30:34 +0800

    Python logging advance example
    Took 40 minutes

---
+++ python.logging/advance_example_1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-  
+import logging
+
+
+def config_with_instance():
+    # create logger
+    logger = logging.getLogger('simple_example')
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    # 'application' code
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warning('warn message')
+    logger.error('error message')
+    logger.critical('critical message')
+
+
+if __name__ == '__main__':
+    config_with_instance()
---
+++ python.logging/advance_example_2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import logging
+import logging.config
+
+
+def config_with_file():
+    logging.config.fileConfig('logging.conf')
+
+    # create logger
+    logger = logging.getLogger('simpleExample')
+
+    # 'application' code
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warning('warn message')
+    logger.error('error message')
+    logger.critical('critical message')
+
+
+if __name__ == '__main__':
+    config_with_file()
