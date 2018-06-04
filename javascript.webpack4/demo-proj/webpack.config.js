39493d8a6e7890189cf517a12cff0f4f9f3339ff
ezirmusitua <jferroal@gmail.com>
2018-06-04 12:21:58 +0800

    Tutorial: Webpack4, how to use sass loader

--- javascript.webpack4/demo-proj/webpack.config.js
+++ javascript.webpack4/demo-proj/webpack.config.js
@@ -1,37 +1,39 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isDev = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: { loader: "babel-loader" }
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{ loader: "html-loader", options: { minimize: true } }
-				]
-			},
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "src/index.html",
-			filename: "index.html"
-		}),
-		new MiniCssExtractPlugin({
-			template: "[name].css",
-			chunkFilename: '[id].css'
-		})
-	]
-};
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {loader: "babel-loader"}
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {loader: "html-loader", options: {minimize: true}}
+        ]
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDev ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDev ? '[id].css' : '[id].[hash].css',
+    })
+  ]
+}
