827db2bb564d3c4e52e85a321a60be8748c7e73a
ezirmusitua <jferroal@gmail.com>
2018-06-04 14:32:13 +0800

    Tutorial: Webpack4, handle image

--- javascript.webpack4/demo-proj/webpack.config.js
+++ javascript.webpack4/demo-proj/webpack.config.js
@@ -1,19 +1,36 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const isDev = process.env.NODE_ENV !== 'production'
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const isDev = process.env.NODE_ENV !== 'production'
+console.log()
 module.exports = {
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {loader: "babel-loader"}
+        use: {loader: 'babel-loader'}
+      },
+      {
+        test: /assets\/.*\.(jpg|png)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'assets/[name].[hash].[ext]',
+          },
+        },
       },
       {
         test: /\.html$/,
         use: [
-          {loader: "html-loader", options: {minimize: true}}
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
         ]
       },
       {
@@ -28,8 +45,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      filename: "index.html"
+      template: 'src/index.html',
+      filename: 'index.html'
     }),
     new MiniCssExtractPlugin({
       filename: isDev ? '[name].css' : '[name].[hash].css',
